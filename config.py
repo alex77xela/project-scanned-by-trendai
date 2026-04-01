37dbcb073fa76529910955660fc3ca76f6dea4f1
alex77xela <61875028+alex77xela@users.noreply.github.com>
2026-04-01 22:54:04 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py - NE JAMAIS FAIRE CELA EN PROD !
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+GITHUB_TOKEN = "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+DB_PASSWORD = "SuperSecret123!"
+STRIPE_API_KEY = "sk_live_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
